4d58cd6a382057e128ec51010b25ad47264ef66e
Amey Badgujar <badgujaramey22@gmail.com>
2022-04-12 15:44:14 +0530

    made changes to scripts

--- script1.sh
+++ script1.sh
@@ -1,3 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" ;
+brew install miniforge ;
 conda install -c apple tensorflow-deps -y ; 
 python -m pip install tensorflow-macos ; 
 python -m pip install tensorflow-metal ; 
