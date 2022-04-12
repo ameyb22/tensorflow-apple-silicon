3dd28320fbd83e22e20f8ab9d40e24f8bcef8640
Amey Badgujar <badgujaramey22@gmail.com>
2022-04-12 15:56:04 +0530

    scripts edited

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+conda create --name tf python=3.9 -y ;
+conda activate tf ;
 conda install -c apple tensorflow-deps -y ; 
 python -m pip install tensorflow-macos ; 
 python -m pip install tensorflow-metal ; 
--- script1.sh
+++ script1.sh
@@ -1,5 +1,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" ;
 brew install miniforge ;
+conda create --name tf python=3.9 -y ;
+conda activate tf ;
 conda install -c apple tensorflow-deps -y ; 
 python -m pip install tensorflow-macos ; 
 python -m pip install tensorflow-metal ; 
