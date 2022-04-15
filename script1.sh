332c6efbdf3313ea3eb0f84baba84cbb2199e8ab
Amey Badgujar <badgujaramey22@gmail.com>
2022-04-16 01:50:44 +0530

    Delete script1.sh

--- script1.sh
+++
@@ -1,8 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" ;
-brew install miniforge ;
-conda create --name tf python=3.9 -y ;
-conda activate tf ;
-conda install -c apple tensorflow-deps -y ; 
-python -m pip install tensorflow-macos ; 
-python -m pip install tensorflow-metal ; 
-conda install numpy pandas matplotlib seaborn scikit-learn ipykernel -y
