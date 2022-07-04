8a9f4f72601d6eaaf5977535e6e5fc4d8884b5b1
Amey Badgujar <badgujaramey22@gmail.com>
2022-07-04 15:03:23 +0530

    python version changed in script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-conda create --name tf python=3.9 -y ;
+conda create --name tf python=3.10 -y ;
 conda activate tf ;
 conda install -c apple tensorflow-deps -y ; 
 python -m pip install tensorflow-macos ; 
